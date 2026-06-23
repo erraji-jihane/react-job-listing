07a752db00bf82ddb926e5b2210d3403f9517889
jihaneerraji025 <jihaneerraji17@gmail.com>
2026-06-23 23:33:50 +0100

    fixed some bugs, added the saved job page

--- src/pages/Saved.jsx
+++ src/pages/Saved.jsx
@@ -1,3 +1,24 @@
-export default function Saved(savedJobs, removeJob) {
-    return <h1>Saved Jobs Page</h1>;
+export default function Saved({savedJobs, removeJob}) {
+    return (
+        <div>
+            <h2>Saved Jobs ({savedJobs.length})</h2>
+            {savedJobs.length === 0 ? (
+                <p>No saved jobs yet</p>
+            ) : (
+                <div>
+                    {savedJobs.map((job) => (
+                    <div key = {job.id}>
+                        <h3>{job.title}</h3>
+                        <p>{job.company}</p>
+                        <p>{job.location}</p>
+                        <button onClick={() => removeJob(job.id)}>
+                            Remove
+                        </button>
+                    </div>
+                    ))}
+                </div>
+                )
+            }
+        </div>         
+    );
 }
