49723b4387d1156d8347407ef1d5623a9763a0af
Jihane Erraji <jihaneerraji17@gmail.com>
2026-06-24 16:53:45 +0100

    style: updated jobs page with search bar aligned to right + styling

--- src/pages/Jobs/Jobs.jsx
+++ src/pages/Jobs/Jobs.jsx
@@ -1,31 +1,29 @@
 import Cards from "../../components/Cards/Cards";
 import './Jobs.css'
 
-export default function Jobs({search,setSearch,savedJobs,setMessage,setSavedJobs,removeJob}) {
-
-    return(
+export default function Jobs({ search, setSearch, savedJobs, setMessage, setSavedJobs, removeJob }) {
+    return (
         <div className="jobs-container">
-   
-            <h2 className="jobs-title">Available Jobs</h2>
-            
-            <div className="searchBar">
-                <input 
-                    type="text"
-                    placeholder="Search a job ..."
-                    value = {search}
-                    onChange={(e)=>setSearch(e.target.value)}/>
+            <div className="jobs-header">
+                <h2 className="jobs-title">Available Jobs</h2>
+                
+                <div className="jobs-search">
+                    <input 
+                        type="text"
+                        placeholder="Search a job ..."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </div>
             </div>
  
             <Cards 
-                search = {search} 
+                search={search} 
                 savedJobs={savedJobs} 
                 setMessage={setMessage} 
                 setSavedJobs={setSavedJobs} 
-                removeJob={removeJob}/>
-
-
-
+                removeJob={removeJob} 
+            />
         </div>
-        
     );
 }
